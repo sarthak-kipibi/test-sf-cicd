924f07c450a9985705405ceaf34e10ed4aaae253
Sarthak Bahuguna <sarthak.k.bahuguna@kipi.bi>
2023-06-06 15:19:36 +0530

    added a view

---
+++ snowflake-objects/views/V1.1.1_create_view.sql
@@ -0,0 +1,14 @@
+/* V_MY_VIEW — description of the view */
+USE DATABASE TEST_CICD;
+USE SCHEMA CICD;
+create or replace secure VIEW V_MY_VIEW as
+select 
+ COLUMN1,
+ COLUMN2
+from FIRST_TABLE_CICD;
+/* adding the view into datashare for secondary account
+ * first switching to role which has privileges to adding objects  into share
+ * then adding grant
+ */
+-- USE ROLE {{ sharing_role }}_{{ env }};
+-- GRANT SELECT ON VIEW V_MY_VIEW TO SHARE MY_DATASHARE_{{ env }};
