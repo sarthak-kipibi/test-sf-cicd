201485c6ae3f2ffb259099e92ce577d664bc4976
Sarthak Bahuguna <sarthak.k.bahuguna@kipi.bi>
2023-06-06 15:25:02 +0530

    updated a view query

--- snowflake-objects/views/V1.1.1_create_view.sql
+++ snowflake-objects/views/V1.1.1_create_view.sql
@@ -3,8 +3,8 @@ USE DATABASE TEST_CICD;
 USE SCHEMA CICD;
 create or replace secure VIEW V_MY_VIEW as
 select 
- COLUMN1,
- COLUMN2
+ COL1,
+ COL2
 from FIRST_TABLE_CICD;
 /* adding the view into datashare for secondary account
  * first switching to role which has privileges to adding objects  into share
